5e1ed3b6c97969a77792fa78aef3d9f7075f2456
Vadim <v.dudko93@gmail.com>
2020-03-15 18:40:01 +0300

    feat: solve morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,46 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
 
+function decode(expr) {
+    let array = [];
+
+    let start = 0;
+    for (let i = 0; i < expr.length/10; i++) {
+    array.push(expr.slice(start, start + 10));
+    start += 10;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        let flag = true;
+        while(flag) {
+            flag = false;
+            if (array[i].includes("10")) {
+                array[i] = array[i].replace("10", ".");
+                flag = true;
+            }
+            if (array[i].includes("11")) {
+                array[i] = array[i].replace("11", "-");
+                flag = true;
+            }
+        }
+        while(array[i].includes("0")) {
+            array[i] = array[i].replace("0", "");
+        }
+
+        for (let key in MORSE_TABLE) {
+            if (array[i] === key) {
+                array[i] = MORSE_TABLE[key];
+                break;
+            }
+        }
+        if (array[i] === "**********"){
+            array[i] = " ";
+        }
+    }
+
+return array.join('');
+}
 module.exports = {
     decode
-}
+};
